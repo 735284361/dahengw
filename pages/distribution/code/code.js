82d1433fb66772e809f4229c2c9ddadf94ab55cc
lu.min <mimi5738@163.com>
2019-11-26 01:07:44 +0800

    分销-第三次提交

--- pages/distribution/code/code.js
+++ pages/distribution/code/code.js
@@ -2,7 +2,9 @@ let app = getApp();
 Page({
 
   data: {
-    
+    imgSrc:'/images/more/timg.jpg',
+    showSaveTip: false,
+    maskStatus: 'hide'
   },
   onLoad: function (options) {
     
@@ -14,9 +16,35 @@ Page({
   onShow: function () {
     
   },
+  longTap: function (e) {
+    // let imgUrl = e.currentTarget.dataset.url;
+    this.setData({
+      showSaveTip: true,
+      maskStatus: 'show'
+    });
+  },
 
-  onUnload: function () {
-    
+  saveOrShare: function () {
+    wx.saveImageToPhotosAlbum({
+      filePath: this.data.imgSrc,
+      success(res) {
+        console.log(res);
+      },
+      fail(res) {
+        console.log(res);
+      }
+    });
+    this.setData({
+      showSaveTip: false,
+      maskStatus: 'hide'
+    });
+  },
+
+  toggleMask: function () {
+    this.setData({
+      maskStatus: 'hide',
+      showSaveTip: false
+    });
   },
 
   onShareAppMessage: function () {
